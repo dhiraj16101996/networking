0dae7642151460f6f9843b17eb3f36e949d0217d
Dhiraj Singh <dhiraj16101996@users.noreply.github.com>
2018-02-16 10:49:56 +0530

    Delete gateway_calc.py

--- gateway_calc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-
-# Finding Gateway ip using route command
-
-gateway = os.popen("ip route show").read()
-gateway.replace("\n","")
-gateway = gateway[11:23]
-print gateway
