887531a179611b75e3be2b4eaa7a1209daabc7b3
Dhiraj Singh <dhiraj16101996@users.noreply.github.com>
2022-04-12 18:10:39 +0530

    Update network-details.py

--- network-details.py
+++ network-details.py
@@ -12,57 +12,57 @@ def network_interface():
 		network = "eth0"
 	elif "wlan1" in route:
 		network = "wlan1"
-	print "\n\nNetwork interface :- ", network
+	print ("\n\nNetwork interface :- ", network)
 
 
 # Finding network it is connected to
 def network_name():
 	name = os.popen("iwgetid -r").read()
 	name = name.replace("\n","")
-	print "Wireless Network name :- ", name
+	print ("Wireless Network name :- ", name)
 
 
 # Finding hostname
 def hostname():
 	host = os.popen("hostname").read()
 	host = host.replace("\n","")
-	print "Host name :- ", host
+	print ("Host name :- ", host)
 
 
 # Finding local ip address
 def local_ip():
-	ip = os.popen("hostname -I").read()
+	ip = os.popen("hostname -I | cut -d ' ' -f 1").read()
 	ip = ip.replace("\n","")
-	print "Local ip :- ", ip
+	print ("Local ip :- ", ip)
 
 
 # Finding default gateway
 def gateway():
-	gate = os.popen("ip route show").read()
+	gate = os.popen("ip route | grep default | head -n 1 | cut -d ' ' -f 3").read()
 	gate = gate.replace("\n","")
-	print "Gateway :- ", gate[11:23]
+	print ("Gateway :- ", gate)
 
 # Main function to run
 def main():
-	print "Finding Network interface.............."
+	print ("Finding Network interface..............")
 	sleep(2)
-	print "Network interface found"
+	print ("Network interface found")
 	sleep(1)
-	print "Finding Network name with which this device is connected........"
+	print ("Finding Network name with which this device is connected........")
 	sleep(2)
-	print "Network name found"
+	print ("Network name found")
 	sleep(1)
-	print "Finding hostname......."
+	print ("Finding hostname.......")
 	sleep(2)
-	print "Hostname found"
+	print ("Hostname found")
 	sleep(1)
-	print "Finding local ip........"
+	print ("Finding local ip........")
 	sleep(2)
-	print "Local ip found"
+	print ("Local ip found")
 	sleep(1)
-	print "Finding default gateway.........."
+	print ("Finding default gateway..........")
 	sleep(2)
-	print "Gateway found"
+	print ("Gateway found")
 	sleep(1)
 	network_interface()
 	network_name()
